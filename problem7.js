51e76e836d50bf5204adab5de0f3011e598be8fc
imrulkaisar <imrulkaisar3@gmail.com>
2023-08-07 16:52:11 +0600

    Solved 7th number

---
+++ problem7.js
@@ -0,0 +1,16 @@
+/**
+ * Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. The program displays a message "Good Work" if the input matches the guess number otherwise "Not matched".
+ */
+
+// solution
+
+/**
+ * Take the guess number return whether match the random number or not
+ * @param {number} input (0 - 10)
+ * @returns {string}
+ */
+function guessTheNumber(input = 0) {
+  let randomNumber = Math.floor(Math.random() * 10 + 1);
+  return randomNumber === input ? "Good Work" : "Not matched";
+}
+console.log(guessTheNumber(7));
